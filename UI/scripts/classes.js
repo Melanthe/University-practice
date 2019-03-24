aa8c2a820c0e4a372075c40cfad1dfea6cb48ac2
yana <yana.bernachkaya@gmail.com>
2019-03-24 15:27:15 +0300

    Rework in classes

--- UI/scripts/classes.js
+++ UI/scripts/classes.js
@@ -100,23 +100,21 @@ class PostList {
 
 	_filterByAuthor(num, author, posts) {
 
-		if (typeof (author) === 'string') {
-
-			let found = [];
-			let count = 0;
-
-			for (let i = 0; (i < posts.length) && (count < num); ++i) {
-				if (posts[i].photo.author === author) {
-					found.push(posts[i]);
-					count++;
-				}
-			}
-			return found;
-
-		} else {
+		if (!(typeof (author) === 'string')) {
 			console.log('Incorrect argument!');
 			return undefined;
 		}
+
+		let found = [];
+		let count = 0;
+
+		for (let i = 0; (i < posts.length) && (count < num); ++i) {
+			if (posts[i].photo.author === author) {
+				found.push(posts[i]);
+				count++;
+			}
+		}
+		return found;
 	}
 
 	_filterByHashtags(num, hashtags, posts) {
@@ -270,3 +268,186 @@ class PostList {
 		return true;
 	}
 }
+
+class OnClick {
+
+	static like(node, post) {
+
+		node.onclick = function () {
+			if (node.dataset.status === '0') {
+				node.dataset.status = '1';
+				node.firstElementChild.textContent = (++post.likes);
+				node.lastElementChild.style.color = '#FFE066';
+			} else {
+				node.dataset.status = '0';
+				node.firstElementChild.textContent = (--post.likes);
+				node.lastElementChild.style.color = '#D0D0D0';
+			}
+		};
+	}
+
+	static openPopup(clickPlace, popup, shadow) {
+		clickPlace.onclick = function () {
+			shadow.hidden = false;
+			popup.style.display = '';
+		};
+	}
+
+	static exitPopup(button, popup, shadow) {
+		button.onclick = function () {
+			shadow.hidden = true;
+			popup.style.display = 'none';
+		};
+	}
+
+	static loadMore(gallery, viewer) {
+
+		let loadMoreButton = document.getElementById('load-more');
+		loadMoreButton.onclick = function () {
+			gallery.getPhotoPosts(this._shown, undefined, this._curFilter).forEach((post) => {
+				viewer.showPost(post);
+			});	
+		};
+	}
+}
+
+class ViewGallery {
+
+	constructor(gallery = new PostList()) {
+		this._shown = 0;
+		this._gallery = gallery;
+		this._curFilter = new Filter();
+	}
+
+	incrementShown(number = 0) {
+
+		if ((typeof (number) !== 'number')) {
+			console.log('Incorrect arguments!');
+			return;
+		}
+		this._shown += number;
+	}
+
+	_createPost(item, container) {
+
+		let photo = document.createElement('div');
+		photo.classList.add('photo');
+		photo.id = item.id;
+		photo.innerHTML = `
+		<img class='pic' src=${item.photo.path} alt='Invalid photo'>
+		<div class='clickPlace'></div>
+		<div class='overlay'>
+			<span class='author'>${item.photo.author}</span>
+			<div class='like' data-status='0'>
+				<span class='numOfLikes'>${item.likes}</span>
+				<button class='heart'><i class='fas fa-heart'></i></button>
+			</div>
+		</div>`;
+		container.appendChild(photo);
+		OnClick.like(container.lastChild.querySelector('.like'), item);
+	}
+
+	_reformDate(date) {
+		let dateOptions = {
+			day: 'numeric',
+			month: 'short',
+			year: 'numeric',
+		};
+		let timeOptions = {
+			hour: 'numeric',
+			minute: '2-digit',
+			hour12: false
+		};
+		return (date.toLocaleDateString('en-US', dateOptions) + ' ' + date.toLocaleTimeString('en-US', timeOptions));
+	}
+
+	_fillHashtags(item, box) {
+
+		let hashtags = box.querySelector('.hashtags');
+		let span;
+		item.hashtags.forEach(function (tag) {
+			span = document.createElement('span');
+			span.classList.add('tag');
+			span.textContent = '#' + tag;
+			hashtags.appendChild(span);
+		});
+	}
+
+	_createPopupBox(item, container) {
+
+		let popupBox = document.createElement('div');
+		popupBox.className = 'popup-box';
+		popupBox.id = item.id;
+		popupBox.style.display = 'none';
+		popupBox.innerHTML = `
+		<div class='pic-box'>
+			<img class='pic' src=${item.photo.path} alt='Invalid photo'>
+		</div>
+		<div class='info-box'>
+			<div class='info-header'>
+				<div class='author'>${item.photo.author}</div>
+				<div class='date'>${this._reformDate(item.date)}</div>
+			</div>
+			<div class='header-buttons'>
+				<button class='edit'></button>
+				<button class='close'></button>
+			</div>
+			<div class='description'>
+				<span class='text'>${item.description}</span>
+			</div>
+			<div class='hashtags'></div>
+			<div class='comments'></div>
+			<textarea id='add-comment' placeholder='Comment this photo...' maxlength='200' rows='2'></textarea>
+			<div class='like' data-status='0'>
+				<span class='numOfLikes'>${item.likes}</span>
+				<button class='heart'>
+					<i class='fas fa-heart'></i>
+				</button>
+			</div>
+		</div>`;
+		this._fillHashtags(item, popupBox);
+		container.appendChild(popupBox);
+		OnClick.like(container.lastChild.querySelector('.like'), item);
+		OnClick.exitPopup(popupBox.querySelector('.close'), popupBox, container);
+	}
+
+	showPost(post) {
+
+		let photos = document.getElementById('photos');
+		this._createPost(post, photos);
+
+		let popup = document.getElementById('popup-photos');
+		this._createPopupBox(post, popup);
+		popup.hidden = true;
+
+		OnClick.openPopup(photos.lastChild.querySelector('.clickPlace'), popup.lastChild, popup);
+	}
+
+	showPhotoPosts(skip = 0, amount = 15) {
+
+		this._gallery.getPhotoPosts(skip, amount, this._curFilter).forEach((post) => {
+			this.showPost(post);
+		});
+
+		this._shown += (amount - skip);
+	}
+
+	removePost(id = 0) {
+		let post = document.getElementById(id);
+		post && post.remove();
+	}
+
+	loadMore() {
+		OnClick.loadMore(this._gallery, this);
+		this._shown += 15;
+	}
+
+	setFilter(filter = new Filter()) {
+
+		if (!(filter instanceof Filter) || !(filter.validateFilter())) {
+			console.log('Incorrect argument!');
+			return;
+		}
+		this._curFilter = filter;
+	}
+}	
